c07ba004398d9fb16e8504ed41ce3d5872ee4364
benyamindsmith <benjamin@alterclimate.com>
2024-09-05 19:17:12 -0400

    Working on LFSRs

--- src/lfsr.cpp
+++ src/lfsr.cpp
@@ -2,9 +2,19 @@
 #include <math.h>
 using namespace Rcpp;
 
-// Wikipedia: https://en.wikipedia.org/wiki/Linear-feedback_shift_register
+//' Linear Feedback Shift Register
+//'
+//' For more information, check out the \href{https://en.wikipedia.org/wiki/Linear-feedback_shift_register}{Wikipedia Page}.
 // [[Rcpp::export]]
-NumericVector lfsr(int seed) {
+NumericVector lfsr_fib(int seed, int n) {
+  return 0;
+}
+
+NumericVector lfsr_galois(int seed, int n){
+  return 0;
+}
+
+NumericVector lfsr_xorshift(int seed, int n){
   return 0;
 }
 
